96b68db9d4170a60390a20352502ea8bc1a76244
Mike Green <nekbet@gmail.com>
2018-01-19 11:25:50 -0700

    add link

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var http = require("http");
+// var https = require("https");
 var express = require('express');
 var app = express();
 var mysql      = require('mysql');
@@ -10,7 +11,7 @@ app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
-var server = app.listen(8000,"127.0.0.1", function() {
+var server = app.listen(8001,"0.0.0.0", function() {
 	var host = server.address().address
 	var port = server.address().port
 
@@ -18,30 +19,36 @@ var server = app.listen(8000,"127.0.0.1", function() {
 
 });
 
-
-app.get('/dates', function (req, res) {
-   db.query('select datedate from dates', function (error, results, fields) {
-	  if (error) throw error;
-	  res.end(JSON.stringify(results));
-	});
-});
-
 var querySpaces = 'select market, company, location_name, address_1, address_2, address_city, address_state, address_zip from spaces_staging;' ;
+
+app.get('/', function (req, res) {
+	res.json({message: 'Nothing to see here'});
+});
 
 app.get('/spaces', function (req, res) {
    db.query(querySpaces, function (error, results, fields) {
 	  if (error) throw error;
 	  // res.end(JSON.stringify(results));
+	  // console.log(results[0].market);
 	  res.json(results);
 	});
 });
 
-var queryMarkets = 'select market, count(company + location_name) from spaces_staging group by market order by market;' ;
+var queryMarkets = 'select market, count(company + location_name) as spaces_in_market from spaces_staging group by market order by market;' ;
 
 app.get('/markets', function (req, res) {
    db.query(queryMarkets, function (error, results, fields) {
 	  if (error) throw error;
 	  // res.end(JSON.stringify(results));
+	  
+	  var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+	  console.log(fullUrl);
+
+	  for (i = 0; i < results.length; i++) {
+	  	console.log(results[i].market);
+		results[i].marketSpacesLink = fullUrl + "/" + results[i].market;
+	  }
+
 	  res.json(results);
 	});
 });
